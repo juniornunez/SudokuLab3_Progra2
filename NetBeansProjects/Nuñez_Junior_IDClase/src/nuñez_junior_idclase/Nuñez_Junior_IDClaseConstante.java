584229ecd7716bd55a85ce9a07306997e4271cf6
Junior Nuñes <Junior Nues@DESKTOP-S4DNI64>
2024-07-28 21:29:52 -0600

    Utilizamos el operador ternario con variables

---
+++ NetBeansProjects/Nuñez_Junior_IDClase/src/nuñez_junior_idclase/Nuñez_Junior_IDClaseConstante.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nuñez_junior_idclase;
+
+/**
+ *
+ * @author Junior Nuñes
+ */
+
+
+
+/*
+Creamos un programa que nos muestre los datos personales, y la nota constante,
+si al nota es mayor que 59.5 esta aprobado, si es menor, esta reprobado.
+*/
+public class Nuñez_Junior_IDClaseConstante {
+    public static void main(String[] args) {
+        int numeroCuenta= 22411198;
+        String aprobado;
+        String nombre="Junior Moises";
+        String apellido="Nuñez Escobar";
+        int edad= 17;
+        double promedio;
+        promedio=99.9;
+        aprobado= promedio >= 59.5? "Aprobado" : "Desaprobado";
+        
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+        System.out.println("Edad: "+edad);
+        System.out.println("Numero de Cuenta: "+numeroCuenta);
+        System.out.println("Promedio: "+promedio);
+        System.out.println("Estado: "+aprobado);
+        
+        
+        
+    }
+}
