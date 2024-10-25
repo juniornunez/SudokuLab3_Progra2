04d64299f23e7aec72e3f94c4ee0442088974233
INKLizz <160358298+INKLizz@users.noreply.github.com>
2024-10-25 15:58:39 -0600

    Resolvedor

---
+++ ComponenteVisual.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package celdasudoku;
+
+/**
+ *
+ * @author Junior Nuñes
+ */
+public interface ComponenteVisual {
+
+    void dibujar();
+
+    void actualizar();
+    
+}
